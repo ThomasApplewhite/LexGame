7179f175b784ba4c97eeb4a55f2d2e24763d7aa7
Game King <thomasa@pop-os.localdomain>
2023-04-13 16:46:23 -0700

    Added GameStoryBeatAdvanced Signal and Reciever to TextMessageAppSlate
    The signal is recieved from the GameDaemon, and is then emitted identically (so that ConversationEntryNodes can recieve it).

--- Scripts_Scenes/PhoneScenes/AppSlates/GameplayAppSlates/TextMessenger/TextMessengerAppSlate.gd
+++ Scripts_Scenes/PhoneScenes/AppSlates/GameplayAppSlates/TextMessenger/TextMessengerAppSlate.gd
@@ -1,9 +1,11 @@
 extends AppSlate
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal GameStorBeatAdvanced(story_beat, frequency)
+var gsb_advanced_signal_name = "GameStorBeatAdvanced" # setget , _get_gsb_advanced_signal_name
+
+var gsb_advanced_reciever_name = "_on_game_story_beat_advanced" setget , _get_gsb_advanced_reciever_name
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +16,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _get_gsb_advanced_reciever_name() -> String:
+	return gsb_advanced_reciever_name
+
+func _on_game_story_beat_advanced(story_beat, story_beat_frequency: int):
+	emit_signal(gsb_advanced_signal_name, story_beat, story_beat_frequency)
